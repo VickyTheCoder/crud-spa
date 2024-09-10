8306cb3c8b0af3e23502260b1e924e39be51cbc7
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-10 23:29:54 +0530

    URL mapping for add endpoint done

--- Contacts/urls.py
+++ Contacts/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from UI import views
-
+from UI import views as ui
+from Contact import views as ct
 urlpatterns = [
-    path('', views.home_page),
+    path('', ui.home_page),
+    path('contact/insert', ct.add_person),
 ]
