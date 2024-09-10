b80c0fa59a7d477049056bc0a239c79905117192
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-10 22:29:10 +0530

    Setting empty home page

--- UI/views.py
+++ UI/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home_page(request):
+    if request.method == 'GET':
+        return render(request=request, 
+                      template_name='index.html')
+    return HttpResponse("Wrong Request")
