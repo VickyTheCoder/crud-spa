c0360a33d75a8851caf409c6dfb2e4b989d58424
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-10 23:03:45 +0530

    Adding form in add page, to give details for db inserts

--- Contact/models.py
+++ Contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    aadhar = models.CharField(max_length=16, primary_key=True)
+    name = models.CharField(max_length=50)
+    dob = models.DateField()
+    email = models.EmailField()
+    mobile = models.CharField(max_length=30)
+    dp_pic = models.ImageField(upload_to="images/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
--- UI/views.py
+++ UI/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from Contact.forms import PersonForm
 
 # Create your views here.
 def home_page(request):
     if request.method == 'GET':
         return render(request=request, 
-                      template_name='index.html')
+                      template_name='index.html',
+                      context={'add_form': PersonForm})
     return HttpResponse("Wrong Request")
